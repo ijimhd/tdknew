0c6bbc1ab1cfe5df196e8ba973fc68a87a84e032
ijimhd <karanikolosd@gmail.com>
2026-04-08 13:20:20 +0300

    Fixed dropdown menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,13 +1,16 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
+/* Pass the 'e' (event) into the function */
+function myFunction(e) {
+  // Prevent the link from jumping the page to the top
+  if (e) e.preventDefault(); 
+  
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
+  // We check if the click was NOT on the toggle link
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+    var myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
